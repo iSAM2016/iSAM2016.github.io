7869ba3a2cdaa3a3f6c41016260f1686caafbb7e
isam2016 <suhailong@shouzan365.com>
2018-12-28 17:38:09 +0800

    异步

--- content/learn/node/fs/fs.js
+++ content/learn/node/fs/fs.js
@@ -1,24 +1,54 @@
 let fs = require('fs');
 let path = require('path');
 // 前使用 fs.stat() 检查文件是否存在
-//  readdirSync 读取文件夹下的内容
+//  readdirSync 同步读取文件夹下的内容 fs.readdir 异步
 
-// fs
-// r
+// function removeDirSync(dir) {
+//     let stateObj = fs.statSync(dir);
+//     if (stateObj.isDirectory()) {
+//         // 子目录
+//         fs.readdirSync(dir).forEach(_ => {
+//             let path = path.join(dir, _);
+//             removeDirSync(path.join(path));
+//         });
+//         // 删除自己
+//         fs.rmdirSync(dir);
+//     } else {
+//         fs.unlinkSync(dir);
+//     }
+// }
 
-// 删除一个文件夹要读取文件夹内容，fs.readdir
-
-function removeDirSync(path) {
-    let stateObj = fs.statSync(path);
-    if (stateObj.isDirectory) {
-        // 目录
-        fs.readdirSync.forEach(_ => {
-            removeDirSync(path.join(__dirname, _));
+/**
+ * 异步深度优先
+ *
+ * 异步操作需要回调
+ *
+ * @param {*} dir
+ */
+function removeDir(dir, callback) {
+    let stateObj = fs.statSync(dir);
+    if (stateObj.isDirectory()) {
+        fs.readdir(dir, (err, files) => {
+            let paths = files.map(_ => path.join(dir, _));
+            console.log(paths);
+            // 目录下的文件没有了
+            function next(index) {
+                if (index === paths.length) return fs.rmdir(dir, callback);
+                //  当前操作目录
+                let currentPath = paths[index];
+                removeDir(currentPath, () => {
+                    next(index + 1);
+                });
+            }
+            next(0);
         });
     } else {
+        fs.unlink(dir, callback);
     }
 }
-
+removeDir(path.join(__dirname, './a'), () => {
+    console.log('remove success');
+});
 // fs.readdir(path.join(__dirname, '../../../js'), (err, files) => {
 //     let pathstr = files.map(_ => path.join(__dirname, '../../../js/') + _);
 //     console.log(pathstr);
