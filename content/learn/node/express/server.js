cbd34c1d10f3b4a479c67fb08435f8c600c0e896
isam2016 <suhailong@shouzan365.com>
2019-03-08 14:44:24 +0800

    regexp

--- content/learn/node/express/server.js
+++ content/learn/node/express/server.js
@@ -16,16 +16,9 @@ app.post('/', function (req, res) {
 });
 //  路径参数路由， 在路径参数 /user/:name/:id  => /user/1/2 => {name:1,id:2} => req.params
 app.get('user/:name/:id', function (req, res) {
-    req.end(JSON.stringify(req.params));
+    res.end(JSON.stringify(req.params));
 });
 // app.all('*', function (req, res) {
 //     res.end('post home');
 // });
 app.listen(3000);
-
-let path = 'user/:name/:id';
-let real = '/user/1/2';
-
-path.replace(/:([^\/]*)/g, function () {
-    param.push();
-});
